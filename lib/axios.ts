6df0c3340d467ee04dad17fd0b54977de78987d1
Sanjay Saravanan <sanjays@AD.DATATEMPLATE.COM>
2025-12-24 18:10:58 +0530

    updated axios.js

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://appointment-backend-qntn.onrender.com/api";
+const BASE_URL = process.env.BASE_URL || "https://appointment-backend-qntn.onrender.com/api";
 
 const instance = axios.create({
   baseURL: BASE_URL,
