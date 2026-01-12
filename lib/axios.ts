05928a381c32f78673bafd2fb0211dd6e9d6a643
Sanjay Saravanan <sanjays@AD.DATATEMPLATE.COM>
2026-01-12 10:36:28 +0530

    Updated Url

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const localUrl = "http://localhost:5000/api";
-const BASE_URL =
-  window.location.hostname === "localhost"
-    ? localUrl
-    : "https://appointment-backend-qntn.onrender.com/api";
-
+export const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL!;
 
 const instance = axios.create({
   baseURL: BASE_URL,
